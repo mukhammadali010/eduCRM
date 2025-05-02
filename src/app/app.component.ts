0f8a33ccec658a5a1ca9f1e98eb86bfa63bc0491
mukhammadali010 <muhammadalinosirov34@gmail.com>
2025-05-02 22:06:09 +0500

    testing eslint in pipeline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,5 +9,4 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'EduCRM';
-  
 }
