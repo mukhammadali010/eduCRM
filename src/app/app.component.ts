5f56c4f2cdbd76275555104dfc721195c318090d
mukhammadali010 <muhammadalinosirov34@gmail.com>
2025-05-02 21:43:49 +0500

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'appRoot',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'EduCRM';
+  
 }
