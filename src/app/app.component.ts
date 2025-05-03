b96c6ad123139cf69362b5d4fa151625be66b6fb
mukhammadali010 <muhammadalinosirov34@gmail.com>
2025-05-03 23:17:31 +0500

    configured CD in deploy.yml

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'appRoot',
+  selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
