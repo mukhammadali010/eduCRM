5c694a9625a73d130cb3bac7f75dd1275734ce12
mukhammadali010 <muhammadalinosirov34@gmail.com>
2025-05-30 23:30:14 +0500

    VITE removed from deploy.yml

--- scripts/generate-env.ts
+++ scripts/generate-env.ts
@@ -6,13 +6,13 @@ dotenv.config();
 
 // List of required env variables
 const requiredEnvVars = [
-  'VITE_FIREBASE_API_KEY',
-  'VITE_FIREBASE_AUTH_DOMAIN',
-  'VITE_FIREBASE_PROJECT_ID',
-  'VITE_FIREBASE_STORAGE_BUCKET',
-  'VITE_FIREBASE_MESSAGING_SENDER_ID',
-  'VITE_FIREBASE_APP_ID',
-  'VITE_FIREBASE_MEASUREMENT_ID',
+  'FIREBASE_API_KEY',
+  'FIREBASE_AUTH_DOMAIN',
+  'FIREBASE_PROJECT_ID',
+  'FIREBASE_STORAGE_BUCKET',
+  'FIREBASE_MESSAGING_SENDER_ID',
+  'FIREBASE_APP_ID',
+  'FIREBASE_MEASUREMENT_ID',
 ];
 
 for (const key of requiredEnvVars) {
@@ -24,13 +24,13 @@ const targetPath = './src/environments/environment.prod.ts';
 const envFileContent = `export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: '${process.env['VITE_FIREBASE_API_KEY']}',
-    authDomain: '${process.env['VITE_FIREBASE_AUTH_DOMAIN']}',
-    projectId: '${process.env['VITE_FIREBASE_PROJECT_ID']}',
-    storageBucket: '${process.env['VITE_FIREBASE_STORAGE_BUCKET']}',
-    messagingSenderId: '${process.env['VITE_FIREBASE_MESSAGING_SENDER_ID']}',
-    appId: '${process.env['VITE_FIREBASE_APP_ID']}',
-    measurementId: '${process.env['VITE_FIREBASE_MEASUREMENT_ID']}'
+    apiKey: '${process.env['FIREBASE_API_KEY']}',
+    authDomain: '${process.env['FIREBASE_AUTH_DOMAIN']}',
+    projectId: '${process.env['FIREBASE_PROJECT_ID']}',
+    storageBucket: '${process.env['FIREBASE_STORAGE_BUCKET']}',
+    messagingSenderId: '${process.env['FIREBASE_MESSAGING_SENDER_ID']}',
+    appId: '${process.env['FIREBASE_APP_ID']}',
+    measurementId: '${process.env['FIREBASE_MEASUREMENT_ID']}'
   }
 };`;
 
